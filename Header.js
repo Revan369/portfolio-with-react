8da0d179b311426d7fb8701cf286f6982bf89ab6
Chris Di Luca <chrisdiluca@Chriss-MacBook-Pro.local>
2024-01-30 19:05:40 +0000

    added Header.js to include the navbar in the header

---
+++ Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Header = () => {
+  return (
+    <header>
+      {/* Include the navigation bar in the header */}
+      <Navbar />
+    </header>
+  );
+};
+
+export default Header;
