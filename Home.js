19bd2dd7fff19505b790be600507d94e0532623d
Chris Di Luca <chrisdiluca@Chriss-MacBook-Pro.local>
2024-01-30 19:14:30 +0000

    added basic Home.js with basic info and space for more detailed homepage to be edited

---
+++ Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      {/* Display basic information on the home page */}
+      <h1>Chris Di Luca</h1>
+      {/* Display a headshot or avatar */}
+      <img src="path/to//headshot.jpg" alt=" Avatar image" />
+      {/* Write a brand statement */}
+      <p>exciting catchy slogon or phrase</p>
+      {/* Indicate that this is your portfolio site */}
+      <p>This is my portfolio site.</p>
+    </div>
+  );
+};
+
+export default Home;
